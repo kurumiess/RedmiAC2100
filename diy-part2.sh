f8c85b87df7e4d34b15d5b90b802b74bdad2d2c5
KURUMI <60105584+KURUMIESS@users.noreply.github.com>
2022-06-27 19:49:55 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,8 +10,20 @@
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 
-# Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+#修改登录IP
+sed -i 's/192.168.1.1/192.168.3.1/g' package/base-files/files/bin/config_generate
+#修改主机名
+sed -i 's/OpenWrt/Xiaomi-Router/g' package/base-files/files/bin/config_generate
+#修改型号显示
+sed -i 's/Xiaomi Redmi Router AC2100/Redmi AC2100/g' target/linux/ramips/dts/mt7621_xiaomi_redmi-router-ac2100.dts
+sed -i 's/Xiaomi Mi Router AC2100/Xiaomi AC2100/g' target/linux/ramips/dts/mt7621_xiaomi_mi-router-ac2100.dts
+#修改登录密码为password
+sed -i '/root/croot:$1$u4rpvVlw$oCaTlWcgSII4PSZQMXL9c.:19150:0:99999:7:::' package/base-files/files/etc/shadow
+# 修改默认主题 argon
+rm -rf feeds/luci/themes/luci-theme-bootstrap/
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/' feeds/luci/collections/luci*/Makefile
+
+# 应用
 git clone https://github.com/mchome/openwrt-vlmcsd.git package/vlmcsd
 git clone https://github.com/mchome/luci-app-vlmcsd.git package/luci-app-vlmcsd
 git clone https://github.com/kenzok8/openwrt-packages package/openwrt-packages
